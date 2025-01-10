db42c8364523a5374fe44b12598edb058edb925a
SkeletonKing20 <n.bonn2002@gmail.com>
2025-01-10 13:26:42 +0100

    Initial commit

---
+++ src/main/java/com/example/SharedCalendar/SharedCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.SharedCalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedCalendarApplication.class, args);
+	}
+
+}
