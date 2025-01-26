45014dc294808636903ae1d401678e707f9d84ac
Markus Franzen <markus.franzen@iu-study.org>
2025-01-26 16:30:21 +0100

    Added Dockerfile

---
+++ CreateImage.bat
@@ -0,0 +1,2 @@
+start docker build --platform linux/amd64/v3 -t sharedcalendar-0.0.1.release.jar .
+start docker push mfanzen/sharedcalendar-0.0.1.release:latest
--- src/test/java/com/example/SharedCalendar/SharedCalendarApplicationTests.java
+++ src/test/java/com/example/SharedCalendar/SharedCalendarApplicationTests.java
@@ -8,7 +8,7 @@ class SharedCalendarApplicationTests {
 
 	@Test
 	void contextLoads() {
-		
+		var js = ""
 
 
 	}
